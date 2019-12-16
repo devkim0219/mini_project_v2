2f1edbfb27c0e5088f38880d397677ca566e8396
goatson <goatson@email.mail>
2019-12-16 18:30:48 +0900

    191216 18:30 gs

--- record/urls.py
+++ record/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('recordList', views.recordList, name='recordList'),
     path('pitcherList', views.pitcherList, name='pitcherList'),
     path('hitterList', views.hitterList, name='hitterList'),
-    path('teamyear', views.teamyear, name='teamyear')
+    path('teamyear', views.teamyear, name='teamyear'),
+    path('highlight', views.highlight, name='highlight')
+
 ]
